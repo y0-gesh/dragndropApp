86bd8d54dc932ea0938ecd7a08846ab6a5e28e67
y0-gesh <yogeshtandan25@gmail.com>
2024-06-07 18:02:28 +0530

    outer circle loading animation

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from "react";
-// import "./Loader.css";
-import "./TestCircle.css";
-import uploadImg from "../../assets/upload.svg";
 
 function Loader() {
   const [counter, setCounter] = useState(0);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
